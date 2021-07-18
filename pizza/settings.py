e78586a7fb70a1f82b4e093104dd8e19809403c7
Zaman Ehsani <zamanehsani@gmail.com>
2021-07-18 20:21:06 +0400

    added mobilee view

--- pizza/settings.py
+++ pizza/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p$kk^ylq11+utti@skppm%4!ozxfos*fbpgxi#t8_kv@8l^xeu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.154','pizza-ae.herokuapp.com']
 
