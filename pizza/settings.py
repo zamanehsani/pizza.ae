22ff40595e146888119a5bf958cedab2bcc5b45d
Zaman Ehsani <zamanehsani@gmail.com>
2021-07-22 00:06:22 +0400

    added restframwork and api app

--- pizza/settings.py
+++ pizza/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "website.apps.WebsiteConfig",
     "dashboard.apps.DashboardConfig",
+    "api.apps.ApiConfig",
+    "rest_framework",
 ]
 
 
