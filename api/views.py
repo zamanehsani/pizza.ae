1f1250f31c55d1efe461d6020e773fe073c13f0b
Zaman Ehsani <zamanehsani@gmail.com>
2021-08-10 23:01:49 +0400

    understand the errors

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from dashboard import models
 from api import serializers
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 class AreaView(viewsets.ModelViewSet):
     queryset = models.Areas.objects.all()
