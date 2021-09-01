66390d9721c07630839d3cb575e36e373c38ed2c
Zaman Ehsani <zamanehsani@gmail.com>
2021-09-01 23:46:47 +0400

    added the localstorage

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,6 @@ app_name = "website"
 urlpatterns = [ 
     path('', TemplateView.as_view(template_name="website/index.html")),
     path('menu',views.Menu.as_view(), name="menu"),
-    path('cart/<int:pk>', views.Cart.as_view(), name="cart"),
     path('landing/', views.LandingPage.as_view(), name="landingpage"),
     path('customer/login/', views.LoginCustomerView.as_view(), name="customer_login"),
     path('customer/sign-up/', views.Sign_up_CustomerView.as_view(), name="customer_sign_up"),
--- website/views.py
+++ website/views.py
@@ -12,16 +12,6 @@ class Menu(ListView):
         data['page_title'] = 'menu'
         return data
 
-
-class Cart(DetailView):
-    model = models.Menu
-    template_name = "website/cart.html"
-
-    def get_context_data(self, *args, **kwargs):
-        data = super(Cart, self).get_context_data(*args, **kwargs)
-        data['page_title'] = 'your order'
-        return data
-
 class LandingPage(TemplateView):
     template_name = "website/LandingPage.html"
 
