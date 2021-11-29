2863e078be79e6d23e6e5b5d3ae55efd3b3f0a91
unknown <zamanehsani@gmail.com>
2021-11-29 19:57:22 +0400

    new updates

--- website/urls.py
+++ website/urls.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 from . import views
 app_name = "website"
 urlpatterns = [ 
-    path('', TemplateView.as_view(template_name="website/index.html")),
+    path('landing', TemplateView.as_view(template_name="website/index.html")),
     path('menu',views.Menu.as_view(), name="menu"),
-    path('landing/', views.LandingPage.as_view(), name="landingpage"),
+    path('', views.LandingPage.as_view(), name="landingpage"),
     path('customer/login/', views.LoginCustomerView.as_view(), name="customer_login"),
     path('customer/sign-up/', views.Sign_up_CustomerView.as_view(), name="customer_sign_up"),
     path('cart/', views.CartView.as_view(), name="cart_view"),
